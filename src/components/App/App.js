cc9ccff217e4e7e80641aade25034d24b83b02de
aboborodea <aboborodea@gmail.com>
2020-03-11 11:52:27 -0400

    removing authenticated routes and adding form.js component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,7 @@
 import React, { Component, Fragment } from 'react'
-import { Route } from 'react-router-dom'
 
-import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
 import Header from '../Header/Header'
-import SignUp from '../SignUp/SignUp'
-import SignIn from '../SignIn/SignIn'
-import SignOut from '../SignOut/SignOut'
-import ChangePassword from '../ChangePassword/ChangePassword'
 
 class App extends Component {
   constructor () {
@@ -41,20 +35,6 @@ class App extends Component {
             message={msgAlert.message}
           />
         ))}
-        <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword msgAlert={this.msgAlert} user={user} />
-          )} />
-        </main>
       </Fragment>
     )
   }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const UserForm = (props) => {
+    
+}
+
+export default UserForm
